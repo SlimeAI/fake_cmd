216180a00fba46fb2407a8771d9abaaa694868b5
johncaged <liuzikang0625@gmail.com>
2024-03-25 22:53:59 +0800

    refactor: set shorter session id suffix.

--- fake_cmd/core/__init__.py
+++ fake_cmd/core/__init__.py
@@ -15,7 +15,7 @@ from fake_cmd.utils.comm import Message, CommandMessage, create_symbol
 from fake_cmd.utils.file import remove_dir_with_retry, remove_file_with_retry
 from fake_cmd.utils.logging import logger
 
-SESSION_ID_SUFFIX = '__fake_cmd_session__'
+SESSION_ID_SUFFIX = '__fcmd_sess'
 
 
 class ServerInfo(ReadonlyAttr):
