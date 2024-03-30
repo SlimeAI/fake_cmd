aa47f75f8f2bc1635a6adfb2351a1a2b030ea8bf
johncaged <liuzikang0625@gmail.com>
2024-03-30 14:10:41 +0800

    refactor: add ``NOTHING`` as a readline module adapter.

--- fake_cmd/__init__.py
+++ fake_cmd/__init__.py
@@ -16,7 +16,16 @@ server, so does ``client``.
 __version__ = '0.0.5'
 
 # NOTE: Import ``readline`` here to improve input experience.
+readline_loaded: bool = False
 try:
     import readline
 except Exception:
     pass
+else:
+    readline_loaded = True
+
+if not readline_loaded:
+    # NOTE: Add a ``NOTHING`` adapter here for possible ``readline`` 
+    # API usage in future versions, and if ``readline`` is not installed, 
+    # this allows the API calls to do nothing without exception raised.
+    from slime_core.utils.typing import NOTHING as readline
